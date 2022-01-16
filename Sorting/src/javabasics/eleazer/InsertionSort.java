87ae1ac302fcf22219f28011b1d385618c958976
Eleazer-Afotey <eleazerallnice@gmail.com>
2022-01-16 14:23:06 -0500

    added selection sort and insertion sort algorithms

---
+++ Sorting/src/javabasics/eleazer/InsertionSort.java
@@ -0,0 +1,35 @@
+package javabasics.eleazer;
+
+/*
+Insertion sort algorithm
+ */
+public class InsertionSort {
+
+    public static int[] insertionSortFunction(int[] intArray){
+        int[] array = intArray;
+        for(int firstUnsortedIndex = 1; firstUnsortedIndex<intArray.length; firstUnsortedIndex++){
+            int item = array[firstUnsortedIndex];
+
+            int i = 0;
+            for(i = firstUnsortedIndex; i > 0 && intArray[i-1] > item; i--){
+                intArray[i] = intArray[i-1];
+            }
+            intArray[i] = item;
+        }
+
+        return intArray;
+    }
+
+    public static void main(String[] args){
+        int[] intArray = {4,-2,45, 23, 45, 32, 12, 17, 19, 20, 67, 58, 90};
+        insertionSortFunction(intArray);
+        for(int num: intArray){
+            System.out.print(num + " ");
+        }
+
+
+    }
+
+
+
+}
---
+++ Sorting/src/javabasics/eleazer/SelectionSort.java
@@ -0,0 +1,57 @@
+package javabasics.eleazer;
+
+
+public class SelectionSort {
+
+    /*Selection Sort Algorithm
+
+     */
+    public static int[] selectionSortFunction(int[] intArray){
+
+        int[] array = intArray;
+
+        for(int lastUnsortedIndex = array.length-1; lastUnsortedIndex > 0; lastUnsortedIndex --){
+
+            int largestIndex = 0;
+
+            for(int i = 1; i <= lastUnsortedIndex; i++){
+                if(array[i] > array[largestIndex]){
+                    largestIndex = i;
+                }
+            }
+            swap(intArray,largestIndex,lastUnsortedIndex);
+        }
+
+        return intArray;
+    }
+
+
+    /*SWAP Method for selection sort
+
+     */
+
+    public static void swap(int[] array, int i, int j){
+        if(i == j)
+        {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
+    /*TEST FOR THE METHOD
+     */
+    public static void main(String[] args){
+        System.out.println("This is Quadratic Algorithm O(n^2): \n ");
+        int[] arr = {34, 53, -1, 45, 4, 24, 10, 7, 61, 12, 5, 6};
+        selectionSortFunction(arr);
+
+        for(int num: arr){
+            System.out.print(num + " ");
+        }
+
+    }
+
+}
